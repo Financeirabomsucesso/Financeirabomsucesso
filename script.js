54ff62f4ae2b3821ae2b26e73208b2d3ddf477cd
Financeirabomsucesso <152675613+Financeirabomsucesso@users.noreply.github.com>
2024-02-06 20:39:05 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 function calculateLoan() {
     var creditAmount = parseFloat(document.getElementById('creditAmount').value);
     var installments = parseInt(document.getElementById('installments').value);
-    var interestRate = 0.25; // 5% ao ano
+    var annualInterestRate = 300; // 5% ao ano
+    var monthlyInterestRate = annualInterestRate / 100 / 12;
 
-    var totalAmount = creditAmount * (1 + interestRate);
+    var totalAmount = creditAmount * (1 + monthlyInterestRate);
     var monthlyInstallment = totalAmount / installments;
 
-    document.getElementById('totalAmount').innerHTML = 'Valor total do empréstimo: R$ ' + totalAmount.toFixed(2);
-    document.getElementById('monthlyInstallment').innerHTML = 'Valor da parcela mensal: R$ ' + monthlyInstallment.toFixed(2);
+    if (!isNaN(totalAmount) && !isNaN(monthlyInstallment)) {
+        document.getElementById('result').classList.remove('hidden');
+        document.getElementById('totalAmount').innerHTML = 'Valor total do empréstimo: R$ ' + totalAmount.toFixed(2);
+        document.getElementById('monthlyInstallment').innerHTML = 'Valor da parcela mensal: R$ ' + monthlyInstallment.toFixed(2);
+    } else {
+        // Se não houver resultados, oculte a seção.
+        document.getElementById('result').classList.add('hidden');
+    }
 }
